3399c07bdb774127d88d03c7c6536d4abf1e1c7c
Denis <mugambimuthuri0@gmail.com>
2024-06-09 10:24:03 +0300

    second commit

--- models/author.py
+++ models/author.py
@@ -1,7 +1,41 @@
-class Author:
-    def __init__(self, id, name):
-        self.id = id
-        self.name = name
+import sqlite3
 
-    def __repr__(self):
-        return f'<Author {self.name}>'
+class Author:
+    def __init__(self, name):
+        if not isinstance(name, str):
+            raise TypeError("Name must be of type str")
+        if len(name) == 0:
+            raise ValueError("Name must be longer than 0 characters")
+        
+        self._name = name
+        self._id = None
+        self.save()
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def name(self):
+        return self._name
+
+    def save(self):
+        conn = sqlite3.connect('authors.db')
+        cursor = conn.cursor()
+        
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS authors (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL
+            )
+        ''')
+        
+        cursor.execute('INSERT INTO authors (name) VALUES (?)', (self._name,))
+        self._id = cursor.lastrowid
+        conn.commit()
+        conn.close()
+
+# Example of creating a new author
+new_author = Author("John Doe")
+print(new_author.id)   # Outputs the id of the new author
+print(new_author.name) # Outputs "John Doe"
--- models/magazine.py
+++ models/magazine.py
@@ -1,8 +1,64 @@
-class Magazine:
-    def __init__(self, id, name, category):
-        self.id = id
-        self.name = name
-        self.category = category
+import sqlite3
 
-    def __repr__(self):
-        return f'<Magazine {self.name}>'
+class Magazine:
+    def __init__(self, name, category):
+        if not isinstance(name, str):
+            raise TypeError("Name must be of type str")
+        if not (2 <= len(name) <= 16):
+            raise ValueError("Name must be between 2 and 16 characters")
+        
+        if not isinstance(category, str):
+            raise TypeError("Category must be of type str")
+        if len(category) == 0:
+            raise ValueError("Category must be longer than 0 characters")
+
+        self._id = None
+        self._name = name
+        self._category = category
+
+        self.save()
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Name must be of type str")
+        if not (2 <= len(value) <= 16):
+            raise ValueError("Name must be between 2 and 16 characters")
+        self._name = value
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Category must be of type str")
+        if len(value) == 0:
+            raise ValueError("Category must be longer than 0 characters")
+        self._category = value
+
+    def save(self):
+        conn = sqlite3.connect('magazines.db')
+        cursor = conn.cursor()
+        
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS magazines (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                category TEXT NOT NULL
+            )
+        ''')
+        
+        cursor.execute('INSERT INTO magazines (name, category) VALUES (?, ?)', (self._name, self._category))
+        self._id = cursor.lastrowid
+        conn.commit()
+        conn.close()
